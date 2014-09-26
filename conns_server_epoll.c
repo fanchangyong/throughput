bce4371d23884b7b1b32802d20067b0e447e9081
fanchangyong <fanchangyong@gmail.com>
2014-09-26 14:55:12 +0800

    multi thread model

--- conns_server_epoll.c
+++ conns_server_epoll.c
@@ -19,17 +19,74 @@ void err(char* err)
 	exit(1);
 }
 
+/*
+ * Gobal variables
+ */
 int backlog = 5;
+int epfd = -1;
+int sock = -1;
+int count = 0;
+
+void* worker(void* p)
+{
+	int id = (int)p;
+	printf("thread:%d\n",id);
+	const int max_events = 1024;
+	struct epoll_event events[max_events];
+	for(;;)
+	{
+		int nfds = epoll_wait(epfd,&events,max_events,-1);
+		if(nfds==-1)
+		{
+			err("epoll_awit");
+		}
+		
+		int n=0;
+		for(;n<nfds;n++)
+		{
+			if(events[n].data.fd == sock)
+			{
+				struct sockaddr_in client_addr;
+				socklen_t len = sizeof(client_addr);
+				if(accept(sock,(struct sockaddr*)&client_addr,&len)==-1)
+				{
+					err("accept");
+				}
+				else
+				{
+					char* str_addr = inet_ntoa(client_addr.sin_addr);
+					++count;
+					//printf("[%d] Accepted a client:%s:%d[%d]\n",id,str_addr,client_addr.sin_port,++count);
+				}
+			}
+			else
+			{
+				printf("unknown fd:%d\n",events[n].data.fd);
+				break;
+			}
+		}
+	}
+	return NULL;
+}
+
+void* stat(void* p)
+{
+	for(;;)
+	{
+		printf("%d connections\n",count);
+		sleep(1);
+	}
+}
 
 int main(int argc,char** argv)
 {
-	int sock = socket(AF_INET,SOCK_STREAM,0);
+	sock = socket(AF_INET,SOCK_STREAM,0);
 	if(sock==-1)
 	{
 		err("socket");
 	}
 
-	int epfd = epoll_create(1);
+	epfd = epoll_create(1);
 	if(epfd==-1)
 	{
 		err("epoll_create");
@@ -56,8 +113,7 @@ int main(int argc,char** argv)
 		err("listen");
 	}
 
-	const int max_events = 1024;
-	struct epoll_event ev,events[max_events];
+	struct epoll_event ev;
 	bzero(&ev,sizeof(struct epoll_event));
 	ev.events = EPOLLIN;
 	ev.data.fd = sock;
@@ -71,37 +127,26 @@ int main(int argc,char** argv)
 
 	int count = 0;
 
-	for(;;)
+	
+	pthread_t threads[5];
+	int i;
+	for(i=0;i<4;i++)
 	{
-		int nfds = epoll_wait(epfd,&events,max_events,-1);
-		if(nfds==-1)
+		if(pthread_create(&threads[i],NULL,worker,(void*)i)!=0)
 		{
-			err("epoll_awit");
-		}
-		
-		int n=0;
-		for(;n<nfds;n++)
-		{
-			if(events[n].data.fd == sock)
-			{
-				struct sockaddr_in client_addr;
-				socklen_t len = sizeof(client_addr);
-				if(accept(sock,(struct sockaddr*)&client_addr,&len)==-1)
-				{
-					err("accept");
-				}
-				else
-				{
-					char* str_addr = inet_ntoa(client_addr.sin_addr);
-					//printf("Accepted a client:%s:%d[%d]\n",str_addr,client_addr.sin_port,++count);
-				}
-			}
-			else
-			{
-				printf("unknown fd:%d\n",events[n].data.fd);
-				break;
-			}
+			err("pthread_create");
 		}
 	}
+
+	if(pthread_create(&threads[i],NULL,stat,NULL)!=0)
+	{
+		err("pthread_create");
+	}
+
+	for(i=0;i<sizeof(threads)/sizeof(pthread_t);i++)
+	{
+		pthread_join(threads[i],NULL);
+	}
+
 }
 
